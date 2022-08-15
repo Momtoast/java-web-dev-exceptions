f2d0731c6f3a93c3ccc7162a7ca437e73dfb0741
Melissa Matos <melissamomtoast@gmail.com>
2022-08-15 14:45:56 -0400

    finished the exercises

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,5 +1,9 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
+import java.io.File;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        System.out.println(Divide(4,2));
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,17 +20,40 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        System.out.println(CheckFileExtension(studentFiles.get("Carl")));
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
-        // Write code here!
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return x/y;
+
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        if (fileName == null || fileName.equals("")) {
+            try {
+                throw new FileNameException("You have failed me.");
+            } catch (FileNameException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        } else if (fileName.contains(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
 }
